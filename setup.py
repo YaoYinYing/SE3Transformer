791dda18317ee2bbf8c2d47b4bc34eb779e44122
YaoYinYing <33014714+YaoYinYing@users.noreply.github.com>
2024-05-14 19:20:37 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,4 +8,11 @@ setup(
     description='PyTorch + DGL implementation of SE(3)-Transformers',
     author='Alexandre Milesi',
     author_email='alexandrem@nvidia.com',
+    install_requires=[
+        'e3nn==0.3.3',
+        'wandb==0.12.0',
+        'pynvml==11.0.0',
+        'decorator==5.1.0',
+        'nvtx'
+    ],
 )
